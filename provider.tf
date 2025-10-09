940438872a11f3324f3c4059830986091952640f
Lupfumo <lupfumo.badaga@vodacom.co.za>
2025-10-10 00:03:03 +0200

    add

---
+++ provider.tf
@@ -0,0 +1,23 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "6.14.0"
+    }
+    kubernetes = {
+      source = "hashicorp/kubernetes"
+      version = "~>2.38"
+    }
+  }
+
+  backend "s3" {
+    bucket       = "terraform-bucket-lzb-001"
+    key          = "kubernetes/terraform.tfstate"
+    region       = "ap-south-1"
+    #use_lockfile = true
+  }
+}
+
+provider "aws" {
+ // region = var.aws-region
+}
