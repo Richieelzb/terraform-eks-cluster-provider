48eabe047749e81efc3fa2c79e69d68bd0ce040a
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-01-28 13:02:14 +0200

    create bastion keys

--- key-pair.tf
+++ key-pair.tf
@@ -1,5 +1,5 @@
 
 resource "aws_key_pair" "eks_ssh" {
-  key_name   = "lupfumo-key-mumbai"                         # Name shown in EC2
+  key_name   = "lupfumo-key-mumbai"                          # Name shown in EC2
   public_key = file("${path.module}/lupfumo-key-mumbai.pem") # Reads from current module directory
 }
