be96680cc6f672ecb6601fc7543ced86054d4ce9
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-01-28 15:37:18 +0200

    adding AmazonEKSWorkerNodePolicy policy

--- iam-role-bastion.tf
+++ iam-role-bastion.tf
@@ -24,6 +24,11 @@ resource "aws_iam_role_policy_attachment" "bastion_ssm" {
   policy_arn = "arn:aws:iam::aws:policy/AmazonSSMManagedInstanceCore"
 }
 
+resource "aws_iam_role_policy_attachment" "bastion_ssm" {
+  role       = aws_iam_role.bastion_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AmazonEKSWorkerNodePolicy"
+}
+
 
 resource "aws_iam_instance_profile" "bastion_profile" {
   name = "bastion-eks-instance-profile"
