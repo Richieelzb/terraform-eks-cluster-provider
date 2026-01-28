c6a171f3c3397c2896e8aa31cebed23813069b80
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-01-28 16:51:06 +0200

    deleting to save costs

--- eks-cluster.tf
+++ eks-cluster.tf
@@ -2,7 +2,7 @@ module "eks" {
   source  = "terraform-aws-modules/eks/aws"
   version = "~> 20.0"
 
-  cluster_name    = "my-eks-cluster"
+  cluster_name    = local.eks_cluster_name
   cluster_version = "1.33"
 
   vpc_id     = module.vpc.vpc_id
@@ -33,13 +33,6 @@ module "eks" {
     }
   }
 
-
-
-  # Disable Launch Template for all node groups (module defaults)
-  # eks_managed_node_group_defaults = {
-  #   create_launch_template = false
-  # }
-
   eks_managed_node_groups = {
     private_nodes = {
       desired_size   = 2
@@ -47,11 +40,6 @@ module "eks" {
       max_size       = 3
       instance_types = ["t3.medium"]
       subnet_ids     = module.vpc.private_subnets
-
-      # remote_access = {
-      #   ec2_ssh_key               = data.aws_key_pair.existing-key.key_name
-      #   source_security_group_ids = [aws_security_group.bastion_sg.id] # Only allow from bastion/VPN SG
-      # }
 
     }
   }
