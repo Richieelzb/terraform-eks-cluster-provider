dee1e8e35d24a6b7f91f4f4cb53ad542487e1290
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-01-28 13:17:01 +0200

    create bastion key 1

--- bastion-ec2.tf
+++ bastion-ec2.tf
@@ -4,7 +4,7 @@ resource "aws_instance" "bastion-ec2" {
   instance_type               = var.instance-type
   subnet_id                   = module.vpc.public_subnets[0]
   associate_public_ip_address = true
-  key_name                    = aws_key_pair.bastion.key_name
+  key_name                    = aws_key_pair.eks_ssh.key_name
   vpc_security_group_ids      = [aws_security_group.bastion_sg.id]
 
   tags = { Name = "eks-bastion" }
