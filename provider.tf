e7f46e363c75ff71f072c977c040c1d55a07a695
Lupfumo <lupfumo.badaga@vodacom.co.za>
2026-02-25 22:46:46 +0200

    create eks

--- provider.tf
+++ provider.tf
@@ -11,10 +11,10 @@ terraform {
   }
 
   backend "s3" {
-    bucket       = "terraform-bucket-lzb-001"
-    key          = "kubernetes-provider/terraform.tfstate"
-    region       = "ap-south-1"
-   // use_lockfile = true
+    bucket = "terraform-bucket-lzb-001"
+    key    = "kubernetes-provider/terraform.tfstate"
+    region = "ap-south-1"
+    // use_lockfile = true
   }
 }
 
